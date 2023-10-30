451898d4bf82f02b76fd1638107fcb38ba9a9d5b
leticiamoreiram <leticia.m@gec.inatel.br>
2023-10-30 20:05:53 -0300

    uploading queue constructor

---
+++ src/Queue.js
@@ -0,0 +1,26 @@
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(item) {
+    this.items.push(item);
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return "Fila vazia";
+    }
+    return this.items.shift();
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+}
+
+module.exports = Queue;
