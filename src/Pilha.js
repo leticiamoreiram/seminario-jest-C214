9457df9935de25a29186dd7153a65d38bf943478
Ana Luiza Martins <100813163+AnaLuizaMartins29@users.noreply.github.com>
2023-10-31 00:22:57 -0300

    Inserindo codigos de teste

---
+++ src/Pilha.js
@@ -0,0 +1,38 @@
+class Pilha {
+    constructor() {
+      this.items = [];
+    }
+  
+    push(element) {
+      this.items.push(element);
+    }
+  
+    pop() {
+      if (this.isEmpty()) {
+        return "A pilha está vazia.";
+      }
+      return this.items.pop();
+    }
+  
+    peek() {
+      if (this.isEmpty()) {
+        return "A pilha está vazia.";
+      }
+      return this.items[this.items.length - 1];
+    }
+  
+    isEmpty() {
+      return this.items.length === 0;
+    }
+  
+    size() {
+      return this.items.length;
+    }
+  
+    clear() {
+      this.items = [];
+    }
+  }
+  
+  module.exports = Pilha;
+  
